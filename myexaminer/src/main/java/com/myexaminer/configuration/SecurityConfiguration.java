4578f2a6cdd13e4cea5a7e06da224224294752df
Szymon Szymonowicz <sz.szymonowicz5@gmail.com>
2021-06-17 14:33:04 +0200

    BE Added mappings to CORS and security config

--- myexaminer/src/main/java/com/myexaminer/configuration/SecurityConfiguration.java
+++ myexaminer/src/main/java/com/myexaminer/configuration/SecurityConfiguration.java
@@ -66,6 +66,7 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
                 .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS).and()
                 .authorizeRequests()
                 .antMatchers(HttpMethod.POST, "/account/**").permitAll()
+                .antMatchers(HttpMethod.GET, "/account/unique").permitAll()
                 .anyRequest().authenticated();
 
         http.addFilterBefore(authenticationJwtTokenFilter(), UsernamePasswordAuthenticationFilter.class);
